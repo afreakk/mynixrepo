22acb7141e136fd215f155accd911458483fac38
afreakk <laderud@hotmail.com>
2020-08-08 14:03:45 +0200

    fix grobi

--- pkgs/grobi-afreak/default.nix
+++ pkgs/grobi-afreak/default.nix
@@ -1,17 +1,19 @@
 { stdenv, fetchFromGitHub, buildGoPackage }:
 
-buildGoPackage {
+buildGoPackage rec {
   version = "0.6.0";
   pname = "grobi";
 
   goPackagePath = "github.com/fd0/grobi";
 
   src = fetchFromGitHub {
-    rev = version;
+    rev = "v${version}";
     owner = "fd0";
     repo = "grobi";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "032lvnl2qq9258y6q1p60lfi7qir68zgq8zyh4khszd3wdih7y3s";
   };
+
+  goDeps = ./deps.nix;
 
    meta = with stdenv.lib; {
     homepage = "https://github.com/fd0/grobi";
