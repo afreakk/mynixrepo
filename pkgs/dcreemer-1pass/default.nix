c48cbd2a162e188a4bd8fe4e590dd9630d4b7241
afreakk <laderud@hotmail.com>
2020-08-17 23:52:56 +0200

    cleanup 1pass

--- pkgs/dcreemer-1pass/default.nix
+++ pkgs/dcreemer-1pass/default.nix
@@ -1,6 +1,5 @@
-{ jq, expect, stdenv, fetchFromGitHub, _1password, gnused}:
+{ jq, expect, stdenv, fetchFromGitHub, _1password, gnused, makeWrapper, lib}:
 stdenv.mkDerivation rec {
-  buildInputs = [ jq expect ];
   pname = "dcreemer-1pass";
   version = "1.2";
   src = fetchFromGitHub {
@@ -9,18 +8,11 @@ stdenv.mkDerivation rec {
     repo = "1pass";
     sha256 = "0lchx4rcs8sb17jxfm7mqwlsfq3ip87am2wl7sb04g9c6r2gsg3p";
   };
-  postPatch = ''
-    ${gnused}/bin/sed -i "s@\bexpect\b@${expect}/bin/expect@g" 1pass
-    ${gnused}/bin/sed -i "s@\bjq\b@${jq}/bin/jq@g" 1pass
-    ${gnused}/bin/sed -i "s@\bop\b@${_1password}/bin/op@g" 1pass
-    # quite hard to replace all the gpg stuff in the script. so skip that and install it globally
-    # ${gnused}/bin/sed -i "s@\bgpg\b@$ {gnupg}/bin/gpg@g" 1pass
-    # substituteInPlace 1pass --replace 'expect' '${expect}/bin/expect'
-    # substituteInPlace 1pass --replace 'jq' '${jq}/bin/jq'
-    # substituteInPlace 1pass --replace 'op' '${_1password}/bin/op'
-  '';
+  nativeBuildInputs = [ makeWrapper ];
   installPhase = ''
     mkdir -p $out/bin
-    cp 1pass $out/bin
+    cp 1pass $out/1pass_unwrapped
+    makeWrapper $out/1pass_unwrapped $out/bin/1pass \
+      --prefix PATH : ${lib.makeBinPath [ expect jq _1password gnused ]}
   '';
 }
